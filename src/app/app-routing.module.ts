7d033913b4d973654e10c644ede4315f3d4bd398
Pexilo <67436391+Pexilo@users.noreply.github.com>
2022-05-17 16:51:57 +0200

    404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { HeaderComponent } from './pages/misc/header/header.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SingleUniverseComponent } from './pages/single-universe/single-universe.component';
@@ -8,24 +10,30 @@ import { UniversesComponent } from './pages/universes/universes.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/universes',
-    pathMatch: 'full',
+    component: HeaderComponent,
+    children: [
+      {
+        path: 'universes',
+        component: UniversesComponent,
+      },
+      {
+        path: 'figures',
+        component: FiguresComponent,
+      },
+      {
+        path: 'figure/:idFigure',
+        component: SingleFigureComponent,
+      },
+      {
+        path: 'universe/:idUniverse',
+        component: FiguresComponent,
+      },
+    ],
   },
+
   {
-    path: 'universes',
-    component: UniversesComponent,
-  },
-  {
-    path: 'figures',
-    component: FiguresComponent,
-  },
-  {
-    path: 'figure/:idFigure',
-    component: SingleFigureComponent,
-  },
-  {
-    path: 'universe/:idUniverse',
-    component: FiguresComponent,
+    path: '**',
+    component: NotfoundComponent,
   },
 ];
 
