b602fbfdc45726136400af20f3ede115df5e8cc5
Pexilo <67436391+Pexilo@users.noreply.github.com>
2022-05-18 22:59:58 +0200

    figures ts comments

--- src/app/pages/figures/figures.component.ts
+++ src/app/pages/figures/figures.component.ts
@@ -2,7 +2,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { IFigure } from 'src/app/models/IFigure.model';
 import { AddFigureDialogComponent } from './../misc/add-figure-dialog/add-figure-dialog.component';
 import { NgToastService } from 'ng-angular-popup';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { IUniverse } from './../../models/IUniverse.model';
 import { UniversesService } from './../../services/universes.service';
 import { Component, OnInit } from '@angular/core';
@@ -15,10 +15,11 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./figures.component.scss'],
 })
 export class FiguresComponent implements OnInit {
-  figures: IFigureAndUniverse[] = [];
-  universes!: IUniverse[];
-  universe!: IUniverse;
+  figures: IFigureAndUniverse[] = []; // liste pour l'affichage des figures, interface différente pour ajouter des infos sur l'univers associé
+  universes!: IUniverse[]; // liste des univers, (j'utilise la liste figures dans univers pour afficher mes figurines)
+  universe!: IUniverse; // dans le cas où on a un univers, on récupère les figures de l'univers
 
+  // variables du dialog d'ajout de figurine
   name!: string;
   imageURL!: string;
   idUniverse!: number;
@@ -27,15 +28,20 @@ export class FiguresComponent implements OnInit {
     private universesService: UniversesService,
     private activedRoute: ActivatedRoute,
     public dialog: MatDialog,
-    private toast: NgToastService,
-    private spinner: NgxSpinnerService
+    private toast: NgToastService // module de notifs
   ) {}
 
   ngOnInit(): void {
-    let universeId = this.activedRoute.snapshot.params['idUniverse'];
-    universeId ? this.getByUniverseId(universeId) : this.getUniverses();
+    let universeId = this.activedRoute.snapshot.params['idUniverse']; // récupère l'id de l'univers dans l'url
+    universeId ? this.getByUniverseId(universeId) : this.getUniverses(); // si on a un id, on récupère les figures de l'univers
   }
 
+  /**
+   * la fonction getUniverses() de l'universService, renvoie un observable.
+   * ensuite pour chaque univers, j'accède à chaque figurine.
+   * Pour chaque figurine, je pousse la figurine vers le tableau
+   * figures, et j'ajoute également les propriétés universeName et universeImageURL à la figure
+   */
   getUniverses(): void {
     this.universesService.getUniverses().subscribe({
       next: (res) => {
@@ -54,6 +60,11 @@ export class FiguresComponent implements OnInit {
     });
   }
 
+  /**
+   * la fonction getByUniverseId(id: number) de l'universeService permet d'obtenir l'univers par son id,
+   * puis je map le tableau de chiffres pour inclure le nom et l'imageURL de l'univers
+   * @param {number} id - nombre - l'id de l'univers à obtenir
+   */
   getByUniverseId(id: number): void {
     this.universesService.getUniverseById(id).subscribe({
       next: (res) => {
@@ -70,6 +81,11 @@ export class FiguresComponent implements OnInit {
     });
   }
 
+  /**
+   * la fonction addFigure() permet d'ajouter une figurine à l'univers
+   * Le formulaire est envoyé à l'universService, qui l'ajoute à l'univers
+   * Puis, on récupère les figures de l'univers, et on les ajoute à la liste
+   */
   openAddFigureDialog() {
     const dialogRef = this.dialog.open(AddFigureDialogComponent, {
       width: '270px',
